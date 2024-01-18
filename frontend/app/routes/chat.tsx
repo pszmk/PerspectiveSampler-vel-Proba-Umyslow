6f4d59e69fc84205212399635f8d73ab3c4109d3
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2024-01-18 16:57:05 +0100

    Add components from shadcn/ui

--- frontend/app/routes/chat.tsx
+++ frontend/app/routes/chat.tsx
@@ -1,17 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Chat } from "@/components/chat";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
 };
 
 export default function ChatPage() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>This is the chat page</h1>
-      <div>something</div>
-    </div>
-  );
+	return Chat();
 }
